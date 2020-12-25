a6da61a208809641b87e65ccecc1cfd5310ea170
Houcinos <houcinos.dhieb@gmail.com>
2020-12-25 01:16:37 +0100

    first trial

---
+++ src/JS/questions.js
@@ -0,0 +1,49 @@
+
+// Array of Objects
+const quiz = [
+    {
+        q :'Which of the following is not JavaScript Data Types?',
+        options:['Undefined','Number','Boolean','Float'],
+        answer:3
+    },
+    {
+        q :'Inside which HTML element do we put the JavaScript?',
+        options:['<script>','<head>','<meta>','<style>'],
+        answer:0
+    },
+    {
+        q :'Which of the following is the correct syntax to display "Letsfindcourse" in an alert box using JavaScript?',
+        options:['alert-box("Letsfindcourse");','confirm("Letsfindcourse");','msgbox("Letsfindcourse");','alert("Letsfindcourse")'],
+        answer:3
+    },
+    {
+        q:'What will be the output of the following Javascript code? var string1 = "Letsfindcourse"; var intvalue = 30; alert( string1 + intvalue );',
+        options:['Letsfindcourse 30','30','Letsfindcourse30','Exception'],
+        answer:2
+    },
+    {
+        q:'Among the following, which one is a ternary operator in JavaScript?',
+        options:['#','::','&:','?:'],
+        answer:3
+    },
+    {
+        q:'Which of them is not the looping structures in JavaScript?',
+        options:['for','while','forwhich','dowhile'],
+        answer:2
+    },
+    {
+        q:'What are the types of Pop up boxes available in JavaScript?',
+        options:['Alert','Prompt','Confirm','All of the above'],
+        answer:3
+    },
+    {
+        q:'Javascript string using double quotes is exactly the same as a string using single quotes?',
+        options:['True','false'],
+        answer:0
+    },
+    {
+        q:'Find output of below code,       var a = "20";        var b = a = 30;        document.write(a+b);',
+        options:['Error in Script'," '20'30 ",'2030','50'],
+        answer:1
+    },
+]
